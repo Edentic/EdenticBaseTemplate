a8fffd8d528f33fc23a312eeddb16f171c3656ba
kasperbaago <kapper14@gmail.com>
2013-09-09 10:22:26 +0200

    hAtom tags removed in image

--- image.php
+++ image.php
@@ -15,10 +15,10 @@ get_header();
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-						<h1 class="entry-title"><?php the_title(); ?></h1>
+					<header>
+						<h1><?php the_title(); ?></h1>
 
-						<div class="entry-meta">
+						<div>
 							<?php
 								$metadata = wp_get_attachment_metadata();
 								printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s">%2$s</time></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%8$s</a>', '_s' ),
@@ -41,9 +41,9 @@ get_header();
 						</nav><!-- #image-navigation -->
 					</header><!-- .entry-header -->
 
-					<div class="entry-content">
+					<div class="entryContent">
 
-						<div class="entry-attachment">
+						<div class="entryAttachment">
 							<div class="attachment">
 								<?php
 									/**
@@ -84,7 +84,7 @@ get_header();
 							</div><!-- .attachment -->
 
 							<?php if ( ! empty( $post->post_excerpt ) ) : ?>
-							<div class="entry-caption">
+							<div class="entryCaption">
 								<?php the_excerpt(); ?>
 							</div><!-- .entry-caption -->
 							<?php endif; ?>
@@ -95,7 +95,7 @@ get_header();
 
 					</div><!-- .entry-content -->
 
-					<footer class="entry-meta">
+					<footer class="entryMeta">
 						<?php if ( comments_open() && pings_open() ) : // Comments and trackbacks open ?>
 							<?php printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', '_s' ), get_trackback_url() ); ?>
 						<?php elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open ?>
